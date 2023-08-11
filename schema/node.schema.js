7f181990f33534759109d28196ed9ee0831d97c5
Bill.W <wyi.net@gmail.com>
2023-08-11 17:01:41 +0800

    make the external IP arg optional

--- schema/node.schema.js
+++ schema/node.schema.js
@@ -2,7 +2,7 @@ const Joi = require('joi')
 
 const nodeSchema = Joi.object({
   mode: Joi.string().valid('authority', 'storage', 'watcher').required(),
-  externalIp: Joi.string().required(),
+  externalIp: Joi.string().optional(),
   domainName: Joi.string().allow(''),
   chainWsUrl: Joi.string().required(),
   region: Joi.string().default("en"),
