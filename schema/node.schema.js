e9a0bf9aa94bf4e8c5c833036d922eb0e2d87c0b
Bill.W <wyi.net@gmail.com>
2023-08-15 13:58:49 +0800

    allow external ip config to empty string

--- schema/node.schema.js
+++ schema/node.schema.js
@@ -2,7 +2,7 @@ const Joi = require('joi')
 
 const nodeSchema = Joi.object({
   mode: Joi.string().valid('authority', 'storage', 'watcher').required(),
-  externalIp: Joi.string().optional(),
+  externalIp: Joi.string().allow('').optional(),
   domainName: Joi.string().allow(''),
   chainWsUrl: Joi.string().required(),
   region: Joi.string().default("en"),
