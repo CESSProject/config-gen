062a478b70dd4276b8620855fcee7f48eb54cd70
Demos Chiang <1239865849@qq.com>
2023-12-12 09:40:11 +0000

    feat:add one step config reverse proxy

---
+++ config-gen/nginx-config.gen.js
@@ -0,0 +1,27 @@
+async function genNginxComposeConfigs(config, _) {
+    const ngxCfg = config.nginx;
+    const nginxHostname = "nginx";
+    let nginxVolumeMappings = [`${ngxCfg.confPath}:/etc/nginx/conf.d`];
+    nginxVolumeMappings.push(`${ngxCfg.logPath}/log:/var/log/nginx`);
+
+    return {
+        image: `nginx:latest`,
+        container_name: nginxHostname,
+        hostname: nginxHostname,
+        restart: "always",
+        expose: [80],
+        ports: [`80:80`],
+        networks: ["kaleido"],
+        volumes: nginxVolumeMappings,
+        logging: {
+            driver: "json-file",
+            options: {
+                "max-size": "300m",
+                "max-file": "10",
+            },
+        },
+    }
+}
+module.exports = {
+    genNginxComposeConfigs: genNginxComposeConfigs,
+};
